90e2a7b32e9456b255bdca66b56ee128daa6eebd
asyrafjanai <asyrafjanai@gmail.com>
2020-06-08 15:02:13 +0800

    comment not in use dag

--- dags/user_behaviour.py
+++ dags/user_behaviour.py
@@ -17,9 +17,9 @@ import psycopg2
 # local
 unload_user_purchase ='./scripts/sql/filter_unload_user_purchase.sql'
 temp_filtered_user_purchase = '/temp/temp_filtered_user_purchase.csv'
-# movie_review_local = '/data/movie_review/movie_review.csv'
-# movie_clean_emr_steps = './dags/scripts/emr/clean_movie_review.json'
-# movie_text_classification_script = './dags/scripts/spark/random_text_classification.py'
+movie_review_local = '/data/movie_review/movie_review.csv'
+movie_clean_emr_steps = './dags/scripts/emr/clean_movie_review.json'
+movie_text_classification_script = './dags/scripts/spark/random_text_classification.py'
 
 # remote config
 BUCKET_NAME = 'mat-de'
@@ -100,73 +100,73 @@ remove_local_user_purchase_file = PythonOperator(
     },
 )
 
-movie_review_to_s3_stage = PythonOperator(
-    dag=dag,
-    task_id='movie_review_to_s3_stage',
-    python_callable=_local_to_s3,
-    op_kwargs={
-        'filename': movie_review_local,
-        'key': movie_review_load,
-    },
-)
+# movie_review_to_s3_stage = PythonOperator(
+#     dag=dag,
+#     task_id='movie_review_to_s3_stage',
+#     python_callable=_local_to_s3,
+#     op_kwargs={
+#         'filename': movie_review_local,
+#         'key': movie_review_load,
+#     },
+# )
 
-move_emr_script_to_s3 = PythonOperator(
-    dag=dag,
-    task_id='move_emr_script_to_s3',
-    python_callable=_local_to_s3,
-    op_kwargs={
-        'filename': movie_text_classification_script,
-        'key': 'scripts/random_text_classification.py',
-    },
-)
+# move_emr_script_to_s3 = PythonOperator(
+#     dag=dag,
+#     task_id='move_emr_script_to_s3',
+#     python_callable=_local_to_s3,
+#     op_kwargs={
+#         'filename': movie_text_classification_script,
+#         'key': 'scripts/random_text_classification.py',
+#     },
+# )
 
-with open(movie_clean_emr_steps) as json_file:
-    emr_steps = json.load(json_file)
+# with open(movie_clean_emr_steps) as json_file:
+#     emr_steps = json.load(json_file)
 
-# adding our EMR steps to an existing EMR cluster
-add_emr_steps = EmrAddStepsOperator(
-    dag=dag,
-    task_id='add_emr_steps',
-    job_flow_id=EMR_ID,
-    aws_conn_id='aws_default',
-    steps=emr_steps,
-    params={
-        'BUCKET_NAME': BUCKET_NAME,
-        'movie_review_load': movie_review_load_folder,
-        'text_classifier_script': text_classifier_script,
-        'movie_review_stage': movie_review_stage
-    },
-    depends_on_past=True
-)
+# # adding our EMR steps to an existing EMR cluster
+# add_emr_steps = EmrAddStepsOperator(
+#     dag=dag,
+#     task_id='add_emr_steps',
+#     job_flow_id=EMR_ID,
+#     aws_conn_id='aws_default',
+#     steps=emr_steps,
+#     params={
+#         'BUCKET_NAME': BUCKET_NAME,
+#         'movie_review_load': movie_review_load_folder,
+#         'text_classifier_script': text_classifier_script,
+#         'movie_review_stage': movie_review_stage
+#     },
+#     depends_on_past=True
+# )
 
-last_step = len(emr_steps) - 1
+# last_step = len(emr_steps) - 1
 
-# sensing if the last step is complete
-clean_movie_review_data = EmrStepSensor(
-    dag=dag,
-    task_id='clean_movie_review_data',
-    job_flow_id=EMR_ID,
-    step_id='{{ task_instance.xcom_pull("add_emr_steps", key="return_value")[' + str(
-        last_step) + '] }}',
-    depends_on_past=True,
-)
+# # sensing if the last step is complete
+# clean_movie_review_data = EmrStepSensor(
+#     dag=dag,
+#     task_id='clean_movie_review_data',
+#     job_flow_id=EMR_ID,
+#     step_id='{{ task_instance.xcom_pull("add_emr_steps", key="return_value")[' + str(
+#         last_step) + '] }}',
+#     depends_on_past=True,
+# )
 
-user_purchase_to_rs_stage = PythonOperator(
-    dag=dag,
-    task_id='user_purchase_to_rs_stage',
-    python_callable=run_redshift_external_query,
-    op_kwargs={
-        'qry': "alter table spectrum.user_purchase_staging add partition(insert_date='{{ ds }}') \
-            location 's3://<your-s3-bucket>/user_purchase/stage/{{ ds }}'",
-    },
-)
+# user_purchase_to_rs_stage = PythonOperator(
+#     dag=dag,
+#     task_id='user_purchase_to_rs_stage',
+#     python_callable=run_redshift_external_query,
+#     op_kwargs={
+#         'qry': "alter table spectrum.user_purchase_staging add partition(insert_date='{{ ds }}') \
+#             location 's3://<your-s3-bucket>/user_purchase/stage/{{ ds }}'",
+#     },
+# )
 
-get_user_behaviour = PostgresOperator(
-    dag=dag,
-    task_id='get_user_behaviour',
-    sql=get_user_behaviour,
-    postgres_conn_id='redshift'
-)
+# get_user_behaviour = PostgresOperator(
+#     dag=dag,
+#     task_id='get_user_behaviour',
+#     sql=get_user_behaviour,
+#     postgres_conn_id='redshift'
+# )
 
 pg_unload >> end_of_data_pipeline
 # user_purchase_to_s3_stage >> remove_local_user_purchase_file >> user_purchase_to_rs_stage
